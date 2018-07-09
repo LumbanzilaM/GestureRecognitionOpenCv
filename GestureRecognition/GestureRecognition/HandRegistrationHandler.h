f07fa86e80d9cda4c0c83578984a3b57017a39db
ALTAWAK/m.lumbanzila <m.lumbanzila@scancube.com>
2018-07-09 19:43:55 +0200

    Updated to last version

--- GestureRecognition/GestureRecognition/HandRegistrationHandler.h
+++ GestureRecognition/GestureRecognition/HandRegistrationHandler.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "opencv2/objdetect.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
 #include <opencv2/highgui.hpp>
--- GestureRecognition/GestureRecognition/MyCamImage.h
+++ GestureRecognition/GestureRecognition/MyCamImage.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
 #include <opencv2/highgui.hpp>
